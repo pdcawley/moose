d1f0dd763dfb2ab8a21e7b3533b60fbd056712e0
Dave Rolsky <autarch@urth.org>
2009-12-26 13:11:32 -0600

    Roles have real attributes take 2.
    Now role attributes a separate attribute-like class which knows how to make "real" attributes.

---
+++ lib/Moose/Meta/Mixin/AttributeBase.pm
@@ -0,0 +1,49 @@
+package Moose::Meta::Mixin::AttributeBase;
+
+use strict;
+use warnings;
+
+our $VERSION   = '0.93';
+our $AUTHORITY = 'cpan:STEVAN';
+
+use base 'Class::MOP::Mixin::AttributeBase';
+
+__PACKAGE__->meta->add_attribute( 'isa'  => ( reader => '_isa_metadata' ) );
+__PACKAGE__->meta->add_attribute( 'does' => ( reader => '_does_metadata' ) );
+__PACKAGE__->meta->add_attribute( 'is'   => ( reader => '_is_metadata' ) );
+
+__PACKAGE__->meta->add_attribute( 'required' => ( reader => 'is_required' ) );
+__PACKAGE__->meta->add_attribute( 'lazy'     => ( reader => 'is_lazy' ) );
+__PACKAGE__->meta->add_attribute(
+    'lazy_build' => ( reader => 'is_lazy_build' ) );
+__PACKAGE__->meta->add_attribute( 'coerce' => ( reader => 'should_coerce' ) );
+__PACKAGE__->meta->add_attribute( 'weak_ref' => ( reader => 'is_weak_ref' ) );
+__PACKAGE__->meta->add_attribute(
+    'auto_deref' => ( reader => 'should_auto_deref' ) );
+__PACKAGE__->meta->add_attribute(
+    'type_constraint' => (
+        reader    => 'type_constraint',
+        predicate => 'has_type_constraint',
+    )
+);
+__PACKAGE__->meta->add_attribute(
+    'trigger' => (
+        reader    => 'trigger',
+        predicate => 'has_trigger',
+    )
+);
+__PACKAGE__->meta->add_attribute(
+    'handles' => (
+        reader    => 'handles',
+        writer    => '_set_handles',
+        predicate => 'has_handles',
+    )
+);
+__PACKAGE__->meta->add_attribute(
+    'documentation' => (
+        reader    => 'documentation',
+        predicate => 'has_documentation',
+    )
+);
+
+1;
---
+++ lib/Moose/Meta/Role/Attribute.pm
@@ -0,0 +1,69 @@
+package Moose::Meta::Role::Attribute;
+
+use strict;
+use warnings;
+
+use Carp 'confess';
+use Scalar::Util 'blessed', 'weaken';
+
+our $VERSION   = '0.93';
+our $AUTHORITY = 'cpan:STEVAN';
+
+use base 'Moose::Meta::Mixin::AttributeBase';
+
+__PACKAGE__->meta->add_attribute(
+    'metaclass' => (
+        reader => 'metaclass',
+    )
+);
+
+__PACKAGE__->meta->add_attribute(
+    'associated_role' => (
+        reader => 'associated_role',
+    )
+);
+
+__PACKAGE__->meta->add_attribute(
+    'is' => (
+        reader => 'is',
+    )
+);
+
+__PACKAGE__->meta->add_attribute(
+    'original_options' => (
+        reader => 'original_options',
+    )
+);
+
+sub new {
+    my ( $class, $name, %options ) = @_;
+
+    (defined $name)
+        || confess "You must provide a name for the attribute";
+
+    return bless {
+        name             => $name,
+        original_options => \%options,
+        %options,
+    }, $class;
+}
+
+sub attach_to_role {
+    my ( $self, $role ) = @_;
+
+    ( blessed($role) && $role->isa('Moose::Meta::Role') )
+        || confess
+        "You must pass a Moose::Meta::Role instance (or a subclass)";
+
+    weaken( $self->{'associated_role'} = $role );
+}
+
+sub attribute_for_class {
+    my $self      = shift;
+    my $metaclass = shift;
+
+    return $metaclass->interpolate_class_and_new(
+        $self->name => %{ $self->original_options } );
+}
+
+1;
